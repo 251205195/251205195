ec02f52410f256a1ccb74d727e43291d4508c4a3
251205195 <251205195@ccmailg.meijo-u.ac.jp>
2026-09-02 16:20:36 +0900

    第9講 異なる画面へのデータ送信 画面遷移したときに，遷移前の画面を消去できるようにした。 電卓(main3)で数字等を打ち込めるようにした

--- app/src/main/java/jp/ac/meijou/android/s251205195/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s251205195/MainActivity2.java
@@ -5,10 +5,15 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Optional;
 
 import jp.ac.meijou.android.s251205195.databinding.ActivityMain2Binding;
 import jp.ac.meijou.android.s251205195.databinding.ActivityMainBinding;
@@ -52,5 +57,34 @@ public class MainActivity2 extends AppCompatActivity {
             startActivity(intent);
 
         });
+
+        // Action Buttonが押された時
+        binding.actionButton.setOnClickListener(view ->{
+            var intent = new Intent(this, MainActivity3.class);
+            getActivityResult.launch(intent);
+        });
     }
+
+
+    //ActivityResultLauntcherはonCreateの外に置く
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK:
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result : " + text)
+                                .ifPresent(text -> binding.intentTextView.setText(text));
+                        break;
+                    case RESULT_CANCELED:
+                        binding.intentTextView.setText("Result : Canceled");
+                        break;
+                    default:
+                        binding.intentTextView.setText("Result : unknown(" + result.getResultCode() + ")");
+                        break;
+
+                }
+            }
+    );
 }
--- app/src/main/java/jp/ac/meijou/android/s251205195/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s251205195/MainActivity3.java
@@ -33,6 +33,71 @@ public class MainActivity3 extends AppCompatActivity {
         String sentText = intent.getStringExtra("editText");
         binding.textView.setText(sentText);
 
+        // button_ok
+        binding.buttonOk.setOnClickListener(view->{
+            var ok_intent =new Intent();
+            ok_intent.putExtra("ret","OK");
+            setResult(RESULT_OK,ok_intent);
+            finish();
+        });
+
+        // button_no
+        binding.buttonNo.setOnClickListener(view->{
+            setResult(RESULT_CANCELED);
+            finish();
+        });
+
+        // 電卓の挙動
+        binding.button0.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "0");
+        });
+        binding.button1.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "1");
+        });
+        binding.button2.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "2");
+        });
+        binding.button3.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "3");
+        });
+        binding.button4.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "4");
+        });
+        binding.button5.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "5");
+        });
+        binding.button6.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "6");
+        });
+        binding.button7.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "7");
+        });
+        binding.button8.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "8");
+        });
+        binding.button9.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "9");
+        });
+        binding.buttonAdd.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "+");
+        });
+        binding.buttonSub.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "-");
+        });
+        binding.buttonMul.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "×");
+        });
+        binding.buttonDiv.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "÷");
+        });
+        binding.buttonAc.setOnClickListener(view -> {
+            binding.textView.setText("");
+        });
+
+        binding.buttonEqual.setOnClickListener(view -> {
+            binding.textView.setText(binding.textView.getText() + "");
+        });
+
 
     }
 }
